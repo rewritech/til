03238f36571fce20d3ee26751bd8763dcae0ff1b
GHJ <rwtstar21@gmail.com>
2020-10-08 21:16:35 +0900

    Add typescript env

---
+++ Type_script/dist/index.js
@@ -0,0 +1,22 @@
+// npx ts-node index.ts
+var val1 = 1;
+var val2 = 2;
+val1 = 10;
+val2 = 20;
+var val3 = 3; // set number automatically 
+var a1 = 1;
+var b1 = [];
+var c1 = a1 + b1;
+var a2 = 1;
+var b2 = [];
+var c2 = a2 + b2;
+var x; // 잘 안 씀
+x = 3;
+if (typeof x === 'number') {
+    x += 10;
+}
+var user = {
+    id: 1,
+    house: true
+};
+user['option'] = 'VIP';
