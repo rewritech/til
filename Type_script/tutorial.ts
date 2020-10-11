410529d386f7658674c9784b42f730dbb98c7a37
GHJ <rwtstar21@gmail.com>
2020-10-11 23:05:40 +0900

    Add typescript concept of function

--- Type_script/tutorial.ts
+++ Type_script/tutorial.ts
@@ -17,6 +17,7 @@ let c2 = a2 + b2
 
 let x: unknown  // 잘 안 씀
 x = 3
+x += 10  // need to check with if
 if (typeof x === 'number') {
     x += 10
 }
@@ -66,3 +67,134 @@ type Staff = {
 
 type extreme = User & Staff
 type normal = User | Staff
+
+// tuple
+let tuple1: [number] = [1]
+let tuple2: [number, string] = [1]
+let tuple3: [number] = [1, 2, 3, 4]
+
+let r_a: readonly number[] = [0, 1, 2]
+a[0] = 3  // 불가
+
+
+function add0(a: number, b: number) {  // return type automatically set
+    return a + b
+}
+
+function add1(a: number, b: number): number {
+    return a + b
+}
+
+function add2(a: number, b: number): string {
+    return a + b  // need to return string
+}
+
+let add4 = (a: number, b: number) => a + b
+
+let add5 = (a: number, b: number) => {
+    let x = a + b
+    return x
+}
+
+// type function
+type Add_f = (a: number, b: number) => number
+let add6: Add_f = (a, b) => a + b
+
+
+// paramater
+function fs(t: string, n = 'it', p?: boolean, ...arr:[]) {
+    console.log(t, n, p, arr)
+}
+
+
+// Generic
+type Filter1 = <T>(array: T[], f: (item: T) => boolean) => T[]
+
+let filter1: Filter1 = (array, f) => {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        if (f(array[i])) {
+            result.push(array[i])
+        }
+    }
+    return result
+}
+
+let filter_result1 = filter1([1, 2, 3], n => n > 2)
+
+
+// Generic scope: can set type
+type Filter2<T> = (array: T[], f: (item: T) => boolean) => T[]
+
+let filter2: Filter2<number> = (array, f) => {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        if (f(array[i])) {
+            result.push(array[i])
+        }
+    }
+    return result
+}
+
+let filter_result2 = filter2([1, 2, 3], n => n > 2)
+
+
+// Generic multi type
+type Filter3<T, U> = (array: T[], f: (item: T) => U) => T[]
+
+let filter3: Filter3<number, boolean> = (array, f) => {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        if (f(array[i])) {
+            result.push(array[i])
+        }
+    }
+    return result
+}
+
+let filter_result3 = filter3([1, 2, 3], n => n > 2)
+
+
+// Generic restrict (extends)
+type Filter4<T extends number, U extends boolean> = (array: T[], f: (item: T) => U) => T[]
+
+let filter4: Filter4<number, boolean> = (array, f) => {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        if (f(array[i])) {
+            result.push(array[i])
+        }
+    }
+    return result
+}
+
+let filter_result4 = filter4([1, 2, 3], n => n > 2)
+
+
+// Generic default (=)
+type Filter5<T, U extends string | boolean = boolean> = (array: T[], f: (item: T) => U) => T[]
+
+let filter5: Filter5<number> = (array, f) => {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        if (f(array[i])) {
+            result.push(array[i])
+        }
+    }
+    return result
+}
+
+let filter_result5 = filter5([1, 2, 3], n => n > 2)
+
+
+type Filter6<T extends object> = (a: T) => T
+let filter6_1: Filter6<number[]>
+let filter6_2: Filter6<[string, number]> = (tu) => {
+    let first = tu[0]
+    let second = tu[1]
+    let newFirst = first + String(second)
+    let newSecond = first.length + second
+    return [newFirst, newSecond]
+}
+
+let result_filter6_2 = filter6_2(['10', 0])
